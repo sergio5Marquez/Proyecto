d15af923b5c4fa0c41998d4ab5c660a8e0f78cb6
sergio5Marquez <mzsergio97@gmail.com>
2023-03-01 10:53:28 -0300

    campbios

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './componentes/layout/Header';
-import Nav from './componentes/layout/Nav';
-import Footer from './componentes/layout/Footer';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+import Header from './componentes/Header';
+import Nav from './componentes/Nav';
+import Footer from './componentes/Footer';
+
+import Aventura from './generos/Aventura';
+import Lucha from './generos/Lucha';
+import Rol from './generos/Rol';
+import Estrategia from './generos/Estrategia';
 
 function App() {
   return (
     <div className="App">
-      <>(header)</>
+
+      <BrowserRouter>
+
       <Header/>
-      <>(Nav)</>
-      <Nav></Nav>
-      <>(Footer)</>
-      <Footer/>     
+      <div className='separar'></div>
+      <Nav/>
+      <Routes>
+        <Route path='aventura' element={<Aventura/>}/>
+        <Route path='lucha' element={<Lucha/>}/>
+        <Route path='rol' element={<Rol/>}/>
+        <Route path='estrategia' element={<Estrategia/>}/>
+
+      </Routes>
+      </BrowserRouter>
+      
+      <Footer/>      
 
     </div>
   );
